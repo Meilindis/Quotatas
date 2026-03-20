f55f9985240ba978f54b3a93d3562017637dcd18
Harma Everts <113139633+HarmaEverts@users.noreply.github.com>
2026-03-20 19:39:47 +0100

    Add files via upload

---
+++ word_collections.py
@@ -0,0 +1,57 @@
+# This Python file uses the following encoding: utf-8
+
+positive_adjectives = ["great", "cute", "wonderful", "sweet", "creative", "playful", "exciting" ,"inventive", "clever", "brilliant", "amazing", "awesome", "funny", "beautiful", "handsome", "pretty", "smart", "intelligent", "empathetic", "sensible", "practical", "helpful", "talented", "gorgeous", "caring", "delightful"]
+
+neutral_adjectives = ["bright", "rich", "easy", "fierce", "neutral", "high", "low", "slow", "fast", "tricky", "wet", "dry", "practical", "reasonable", "sensitive", "capable"]
+
+negative_adjectives = ["bad", "difficult", "sour", "salty", "bitter", "ugly", "selfish", "poor", "stupid", "filthy", "dominant", "bossy", "arrogant", "narcissistic", "spiteful", "heartless", "uncaring", "greedy", "ignorant", "terrible", "horrible", "gluttonous", "awful", "boring"]
+
+colours = ["pink", "white", "black", "yellow", "green", "blue", "magenta", "teal", "brown", "orange", "amber", "violet", "purple", "indigo", "red", "turquoise", "peach"]
+
+neutral_nouns_singular = ["big splash", "ad blocker",  "wet blanket",  "besom",  "piano", "clown car", "lantern pole", "candy", "chocolate", "chess game", "peanut", "game", "champagne", "wine", "alcohol", "coffee", "tea", "cup", "pet", "petrichor", "rain", "flower", "table", "chair", "vase", "meadow", "rocket", "stone", "alarm clock", "fridge", "freezer", "laundry", "sun", "moon", "star", "planet", "universe", "time machine", "transporter", "holodeck", "galaxy", "TARDIS", "knife", "fork", "spoon", "cutlery", "plate", "earwax", "cassette player", "computers", "android", "gynoid", "robot", "mind", "brain", "adventure", "tavern"]
+
+neutral_nouns_plural = ["ad blockers",  "wet blankets",  "besoms",  "pianos", "clown cars", "lantern poles", "sweets", "chocolates", "peanuts", "games", "pets", "cups", "flowers", "tables", "chairs", "vases", "meadows", "rockets", "stones", "alarm clocks", "fridges", "freezers", "suns", "moons", "stars", "planets", "universes", "time machines", "transporters", "holodecks", "galaxies", "knives", "forks", "spoons", "plates", "cassette players", "computers", "androids", "gynoids", "robots", "minds", "brains", "adventures", "taverns", "clothes"]
+
+people_singular = ["gnome", "elf", "coal miner", "time traveller", "baker", "explorer", "astronaut", "singer", "supporter", "accountant", "rocket scientist", "captain", "tinkerer", "painter", "artist", "musician", "CEO", "manager", "HR director", "programmer", "teacher", "doctor", "nurse", "dentist", "leprechaun", "orc", "halfling", "goblin", "witch", "wizard", "sorcerer", "sorceress", "king", "queen", "prince", "princess", "rook", "knight", "bishop", "pawn", "emperor", "empress"]
+
+people_plural = ["gnomes", "elves", "coal miners", "time travellers", "bakers", "explorers", "astronauts", "singers", "supporters", "accountants", "rocket scientists", "captains", "tinkerers", "painters", "artists", "musicians", "CEOs", "managers", "HR people", "programmers", "teachers", "doctors", "nurses", "dentists", "leprechauns", "orcs", "halflings", "goblins", "witches", "wizards", "sorcerers", "sorceresses", "kings", "queens", "princes", "princesses", "rooks", "knights", "bishops", "pawns", "emperors", "empresses"]
+
+animals_singular = ["cat", "dog", "monkey", "cow", "rabbit", "pigeon", "lion", "ape", "giraffe", "capybara", "hamster", "stork", "heron", "guinea pig", "sheep", "goat", "pig", "chicken", "snake", "dragon", "tiger", "elephant"]
+
+animals_plural = ["cats", "dogs", "monkeys", "cows", "rabbits", "pigeons", "lions", "apes", "giraffes", "capybaras", "hamsters", "storks", "herons", "guinea pigs", "sheep", "goats", "pigs", "chickens", "snakes", "dragons", "tigers", "elephants"]
+
+neutral_verbs = ["play", "write", "type", "swim", "operate", "weave", "knit", "cut", "try", "make", "program", "work", "sniff", "pet", "stroke", "read", "love", "caress", "touch", "taste", "hear", "see", "understand", "run", "drink", "move", "clean", "rub", "lie", "tickle", "stutter", "impress", "remove"] 
+
+verbs_third_person = ["plays", "writes", "types", "swims", "operates", "weaves", "knits", "cuts", "tries", "makes", "programs", "works", "sniffs", "pets", "strokes", "reads", "undresses", "loves", "caresses", "touches", "tastes", "hears", "sees", "understands", "runs", "drinks", "moves", "cleans", "rubs", "lies", "tickles", "stutters", "impresses", "removes"]
+
+verbs_active = ["player", "writer", "typer", "swimmer", "operator", "weaver", "knitter", "cutter", "trier", "maker", "programmer", "worker", "sniffer", "petter", "stroker", "reader", "lover", "caresser", "toucher", "taster", "hearer", "seer", "understander", "runner", "drinker", "mover", "cleaner", "rubber", "liar", "tickler", "stutterer", "impresser", "remover"] 
+
+verbs_ing_neutral = ["playing", "writing", "typing", "swimming", "operating", "weaving", "knitting", "cutting", "trying", "making", "working", "sniffing", "petting", "stroking", "reading", "loving", "caressing", "touching", "tasting", "hearing", "seeing", "understanding", "running", "drinking", "moving", "cleaning", "rubbing", "lying", "tickling", "stuttering", "impressing", "removing"]
+
+times = ["always", "sometimes", "never", "occasionally", "rarely", "often"]
+
+audiences = ["let the whole world know that ", "tell other people that ", "make sure that everyone knows that ", "tell the entire internet that "]
+
+adverbs = ["wonderfully", "playfully", "deeply", "highly", "terribly", "horribly", "shamefully", "masterfully", "wistfully", "plainly", "ferociously", "interestingly", "inevitably", "inadvertently", "unwittingly", "impressively", "indubitably", "harshly"]
+
+nsfw_nouns_singular = ["cock", "tit", "penis", "porn", "bukkake", "cunt", "foreplay", "titty", "boob", "condom", "dildo", "vibrator", "boobie", "boner", "tittyfuck"]
+
+nsfw_nouns_plural = ["cocks", "tits", "penises", "cunts", "boobs", "titties", "condoms", "dildos", "vibrators", "handcuffs", "boobies", "boners"]
+
+nsfw_adjectives = ["fucked up", "sexy", "naked", "fucking", "moist", "hard", "gay"]
+
+nsfw_verbs = ["fuck", "blow", "undress", "suck", "masturbate", "pound", "make love to"]
+ 
+nsfw_verbs_third_person = ["fucks", "blows", "undresses", "sucks", "masturbates", "pounds", "makes love to"]
+
+nsfw_verbs_active = ["fucker", "blower", "undresser", "sucker", "masturbater", "pounder"]
+
+nsfw_verbs_ing = ["fucking", "blowing", "undressing", "sucking", "masturbating", "pounding", "making love to"]
+
+# Aggregated by function (does not include NSFW)
+nouns_singular = neutral_nouns_singular + people_singular + animals_singular + verbs_active
+nouns_plural = animals_plural + people_plural + neutral_nouns_plural
+adjectives = positive_adjectives + neutral_adjectives
+verbs = neutral_verbs
+verbs_3p = verbs_third_person
+verbs_ing = verbs_ing_neutral
